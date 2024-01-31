7aeb3ccd00e650ad3bfb2f08dd15b6c0adce25f9
01Dri <drikill01@gmail.com>
2024-01-31 16:47:51 -0300

    Handler command

--- main.go
+++ main.go
@@ -9,17 +9,19 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
+var botID string
+
 func main() {
 	enviromentVariableToken := "TOKEN_DISCORD_INIT_RULES"
 	var token string = os.Getenv(enviromentVariableToken)
 	dg, err := discordgo.New(token)
-
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	// dg.AddHandler(messageCreate)
-	dg.Identify.Intents = discordgo.IntentsGuildMessages
+	dg.AddHandlerOnce(onReady)
+	dg.Identify.Intents = discordgo.IntentGuildMessages
+	dg.AddHandler(messageCreate)
 	err = dg.Open()
 	if err != nil {
 		fmt.Println("error opening connection", err)
@@ -31,4 +33,27 @@ func main() {
 	<-sc
 	dg.Close()
 
+}
+func onReady(bot *discordgo.Session, event *discordgo.Ready) {
+	botID = bot.State.User.ID
+	fmt.Println("Bot is ready. Bot ID:", botID)
+}
+
+func messageCreate(bot *discordgo.Session, message *discordgo.MessageCreate) {
+	fmt.Printf("Author: %s (%s)\n", message.Author.Username, message.Author.ID)
+	fmt.Printf("Content: %s\n", message.Content)
+
+	if message.Author.ID == botID {
+		return
+	}
+
+	if message.Content == "!start" {
+		fmt.Println(message.Content)
+		_, err := bot.ChannelMessageSend(message.ChannelID, "hello, world!!")
+		if err != nil {
+			fmt.Println(err)
+		}
+	} else {
+		fmt.Println("nao ta lendo a mensagem n dog")
+	}
 }
