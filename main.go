a2218260c0da61d26833b30d70571c7a2e6c8545
01Dri <drikill01@gmail.com>
2024-01-31 15:24:16 -0300

    Starting connection with bot

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	enviromentVariableToken := "TOKEN_DISCORD_INIT_RULES"
+	var token string = os.Getenv(enviromentVariableToken)
+	dg, err := discordgo.New(token)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// dg.AddHandler(messageCreate)
+	dg.Identify.Intents = discordgo.IntentsGuildMessages
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection", err)
+		return
+	}
+	fmt.Println("initRules is now running")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+	dg.Close()
+
 }
